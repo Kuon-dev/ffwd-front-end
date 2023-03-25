de076f9ca889d226c7b3b9c0eb93deab46ec54dd
Hon <126381987+jenghon@users.noreply.github.com>
2023-03-25 20:31:40 +0800

    refactor: modify ts config, vit config, added plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import vue from '@vitejs/plugin-vue';
 // import path from 'path';
 import { fileURLToPath, URL } from 'url';
 
-const setAlias = (alias: String, path: String) => {
+import VueRouter from 'unplugin-vue-router/vite'
+
+
+
+const setAlias = (alias: string, path: string) => {
 	return {
 		find: alias,
 		replacement: fileURLToPath(new URL(path, import.meta.url)),
@@ -15,19 +19,23 @@ export default defineConfig({
 	resolve:{
 		alias:[
 			setAlias('@', './src'),
-			setAlias('@stores', './src/stores/'),
-			setAlias('@admins', './src/views/Admin/'),
-			setAlias('@users', './src/views/User/'),
-			setAlias('@views', './src/views/'),
-			setAlias('@base-components', './src/components/BaseComponents/'),
-			setAlias('@form-components', './src/components/FormComponents/'),
-			setAlias('@nav-components', './src/components/NavigationComponents/'),
-			setAlias('@payment-components', './src/components/PaymentComponents/'),
-			setAlias('@jquery', './src/jquery/'),
+			setAlias('stores', './src/stores/'),
+			setAlias('admins', './src/views/Admin/'),
+			setAlias('users', './src/views/User/'),
+			setAlias('views', './src/views/'),
+			setAlias('base-components', './src/components/BaseComponents/'),
+			setAlias('form-components', './src/components/FormComponents/'),
+			setAlias('nav-components', './src/components/NavigationComponents/'),
+			setAlias('payment-components', './src/components/PaymentComponents/'),
+			setAlias('jquery', './src/jquery/'),
 		],
 	},
 	plugins: [
+		VueRouter({
+			/* options */
+		  }),
 		vue(),
+		
 	],
 
 	server: {
