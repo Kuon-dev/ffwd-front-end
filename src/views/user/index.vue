1a6aa235f84f093cdc4eed57ce89e88fe0d4f846
Hon <126381987+jenghon@users.noreply.github.com>
2023-04-12 09:58:22 +0800

    Fix file
    fix one typo and some .ts file to .vue file with required code

---
+++ src/views/user/index.vue
@@ -0,0 +1,10 @@
+<template>
+	<div></div>
+</template>
+
+<script setup lang="ts">
+// Import { ref } from 'vue'
+// import Test from 'jquery-components/Example';
+</script>
+
+<style lang="scss" scoped></style>
