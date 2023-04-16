f85cfa8c795e8277fcf5e974e6d3edde396d0d63
Kuon-dev <aaronlyn88@gmail.com>
2023-04-16 17:00:32 +0800

    feat: added auth pages

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,79 @@
-<script setup lang="ts"></script>
-
 <template>
-	<div>
+	<div class="flex-col w-full min-h-screen">
 		<Suspense>
-			<router-view />
+			<template #default>
+				<router-view />
+			</template>
+			<template #fallback>
+				<LoadingPage />
+			</template>
 		</Suspense>
 	</div>
 </template>
 
-<style scoped>
-.logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
-	transition: filter 300ms;
+<script setup lang="ts">
+import LoadingPage from 'base-components/BaseLoadingPage.vue';
+
+import 'animate.css';
+import { ref, watch, onMounted } from 'vue';
+
+const windowWidth = ref(window.innerWidth);
+const handleResize = () => {
+	windowWidth.value = window.innerWidth;
+};
+const isInMobile = ref(windowWidth.value < 1024);
+/*
+interface Theme {
+  bg: string,
+  bg2: string,
+  text: string,
+  subtext: string,
+  subtext2: string,
+  panel: string,
+  overlay: string,
+  brand: string,
+  brand2: string,
 }
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
+*/
+
+// const app = ref(document.getElementById('app')?.style);
+
+watch(windowWidth, () => {
+	isInMobile.value = windowWidth.value < 1024;
+});
+
+onMounted(() => {
+	window.addEventListener('resize', handleResize);
+});
+</script>
+
+<style>
+#app {
+	max-width: 1980px;
+	width: 100%;
+	overflow: hidden !important;
+	display: flex;
+	justify-content: center;
+	align-content: start;
 }
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
+
+.div-center {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+::-webkit-scrollbar-track {
+	background-color: transparent;
+}
+
+::-webkit-scrollbar-thumb {
+	background-color: #60246c;
+	border-radius: 20px;
+	border: 6px solid transparent;
+	background-clip: content-box;
+}
+::-webkit-scrollbar {
+	width: 20px;
 }
 </style>
---
+++ src/views/verify-email.vue
@@ -0,0 +1,148 @@
+<template>
+	<div class="div-center h-full base-bg relative">
+		<div class="img-overlay"></div>
+		<Suspense>
+			<template #default>
+				<div
+					class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8 bg-white rounded-lg drop-shadow-md"
+				>
+					<div
+						class="mx-auto max-w-lg text-center"
+						v-if="store.user || store.errorList.status"
+					>
+						<h1 class="text-2xl font-bold sm:text-3xl">
+							{{
+								store.user
+									? 'Verify your email'
+									: `Error ${store.errorList.status} : ${store.errorList.message} `
+							}}
+						</h1>
+						<p class="mt-4 text-gray-500" v-if="store.user">
+							We have sent an email to
+							<span class="font-bold"> {{ userEmail }}</span
+							>. to verify your email address and activate your account.
+							<br />
+							<br />
+							<button
+								class="text-sky-500"
+								@click="handleResendEmailVerification()"
+							>
+								click here
+							</button>
+							if you have not received the email.
+						</p>
+
+						<p class="mt-4 text-gray-500" v-else>Please login again</p>
+					</div>
+
+					<div v-if="!store.user">
+						<div class="flex justify-center pt-10">
+							<button
+								class="inline-block rounded-lg bg-brand px-5 py-3 text-sm font-medium text-white"
+							>
+								<router-link :to="'/login'">
+									<font-awesome-icon icon="fa-regular fa-square-caret-left" />
+									Back to login page
+								</router-link>
+							</button>
+						</div>
+					</div>
+				</div>
+			</template>
+			<template #fallback>
+				<div
+					class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8 bg-white rounded-lg drop-shadow-md"
+				>
+					<div class="mx-auto max-w-lg text-center">
+						<h1 class="text-2xl font-bold sm:text-3xl">Verify your email</h1>
+						<p class="mt-4 text-gray-500">
+							{{ store.authUser }}
+						</p>
+					</div>
+
+					<div class="flex items-center justify-between">
+						<button
+							type="submit"
+							@click="handleForgotPassword($event)"
+							class="inline-block rounded-lg bg-brand px-5 py-3 text-sm font-medium text-white"
+						>
+							Enter
+						</button>
+
+						<p class="text-sm text-gray-500">
+							No account?
+							<router-link :to="'/signup'" class="underline" href=""
+								>Sign up</router-link
+							>
+						</p>
+					</div>
+					<div class="flex justify-end">
+						<router-link :to="'/login'">
+							<font-awesome-icon icon="fa-regular fa-square-caret-left" />
+							Back
+						</router-link>
+					</div>
+				</div>
+			</template>
+		</Suspense>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, computed } from 'vue';
+import { useUserStore } from 'stores/UserStore';
+
+const store = useUserStore();
+console.log(store);
+
+const emailError = ref();
+const responseStatus = ref();
+
+// const backendAPI = ref(import.meta.env.VITE_DEV_API)
+const userEmail = computed(() => {
+	return (store.user as any).email;
+});
+
+const email = ref('');
+
+const handleForgotPassword = async (e: Event) => {
+	e.preventDefault();
+	emailError.value = '';
+	responseStatus.value = '';
+	const status = await store.handleForgotPassword(email.value);
+};
+
+const handleResendEmailVerification = () => {
+	store.handleResendEmailVerification();
+};
+
+const hasLoaded = ref();
+
+onMounted(async () => {
+	const user = await store.getUser();
+	hasLoaded.value = user;
+});
+</script>
+
+<style scoped>
+.base-bg {
+	background-image: url('/images/house7.jpg');
+	background-position: center;
+	background-size: cover;
+	background-repeat: no-repeat;
+}
+
+.img-overlay {
+	position: absolute;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	width: 100%;
+	background-color: #000000;
+	opacity: 0.25;
+	overflow: hidden;
+	height: 100%;
+	transition: 0.5s ease;
+}
+</style>
