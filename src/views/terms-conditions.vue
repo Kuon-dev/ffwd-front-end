ec976030e19c5ef341592295123541665037c2b3
Hon <126381987+jenghon@users.noreply.github.com>
2023-04-17 21:54:21 +0800

    edited term&conditin and index.vue
    added term and condition content in termandcondition.vue and navbar, footer and score dashboard in index.vue

--- src/views/terms-conditions.vue
+++ src/views/terms-conditions.vue
@@ -1,5 +1,53 @@
 <template>
-	<div>terms and conditions</div>
+	<div>
+		Terms and Conditions for CodeGenius Online Coding Learning Platform Welcome
+		to CodeGenius, an online coding learning platform for beginners. Please read
+		these terms and conditions carefully before using the platform. By using the
+		platform, you agree to be bound by these terms and conditions. Registration
+		and Use of Platform 1.1 Eligibility You must be at least 18 years old or
+		have the consent of your parent or legal guardian to use the platform. 1.2
+		Registration To use the platform, you must create an account and provide
+		accurate and complete information. You are responsible for maintaining the
+		confidentiality of your account and password and for all activities that
+		occur under your account. 1.3 Use of Platform You agree to use the platform
+		only for lawful purposes and in accordance with these terms and conditions.
+		You may not use the platform to: Engage in any activity that violates any
+		law or regulation. Transmit any viruses or other harmful materials. Use the
+		platform to send unsolicited messages or spam. Impersonate any person or
+		entity. Attempt to gain unauthorized access to any part of the platform.
+		Modify, adapt, translate, or reverse engineer any portion of the platform.
+		Intellectual Property The platform and its entire contents, features, and
+		functionality are owned by CodeGenius and its licensors and are protected by
+		international copyright, trademark, patent, trade secret, and other
+		intellectual property or proprietary rights laws. User Content 3.1 License
+		You retain all ownership rights to any content you submit to the platform,
+		but you grant CodeGenius a non-exclusive, royalty-free, worldwide,
+		perpetual, and irrevocable license to use, reproduce, modify, adapt,
+		publish, translate, create derivative works from, distribute, perform, and
+		display such content in connection with the platform. 3.2 Responsibility You
+		are solely responsible for any content you submit to the platform. You
+		warrant that: You own or have the necessary rights to submit such content.
+		Such content does not infringe any third party's intellectual property or
+		proprietary rights. Such content does not violate any law or regulation. 3.3
+		Monitoring CodeGenius reserves the right to monitor any content submitted to
+		the platform and to remove any content in its sole discretion. Disclaimers
+		THE PLATFORM AND ITS CONTENT ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+		KIND, EITHER EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+		WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR
+		NON-INFRINGEMENT. Limitation of Liability UNDER NO CIRCUMSTANCES SHALL
+		CODEGENIUS OR ITS AFFILIATES, LICENSORS, OR SERVICE PROVIDERS BE LIABLE FOR
+		ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, CONSEQUENTIAL, OR EXEMPLARY
+		DAMAGES, INCLUDING, BUT NOT LIMITED TO, DAMAGES FOR LOSS OF PROFITS,
+		GOODWILL, USE, DATA, OR OTHER INTANGIBLE LOSSES, ARISING OUT OF OR IN
+		CONNECTION WITH YOUR USE OF THE PLATFORM. Indemnification You agree to
+		indemnify, defend, and hold harmless CodeGenius and its affiliates,
+		licensors, and service providers from and against any claims, liabilities,
+		damages, judgments, awards, losses, costs, expenses, or fees (including
+		reasonable attorneys' fees) arising out of or in connection with your use of
+		the platform or any violation of these terms and conditions. Modifications
+		CodeGenius reserves the right to modify these terms and conditions at any
+		time in its sole discretion. Your continued use of the platform
+	</div>
 </template>
 
 <script setup lang="ts"></script>
