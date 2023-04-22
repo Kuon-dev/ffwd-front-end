92f3bfd181eccf3f3c98c761e74a82c6af0ef56c
Kuon-dev <aaronlyn88@gmail.com>
2023-04-22 14:40:00 +0800

    feat: added course page design, dynamic rendering course card contents

--- src/components/CourseComponents/CourseCard.vue
+++ src/components/CourseComponents/CourseCard.vue
@@ -1,17 +1,19 @@
 <template>
-	<v-card class="card" min-width="300">
+	<v-card class="card flex items-around" min-width="300">
 		<v-img class="align-end" height="200" :src="props.image" cover />
 
 		<v-card-title class="text-brand font-bold"> {{ props.title }}</v-card-title>
 
-		<v-card-text>
+		<v-card-text class="h-32">
 			<p class="text-black">
 				{{ props.cardText }}
 			</p>
 		</v-card-text>
 
-		<v-card-actions>
-			<v-btn color="#7E81FF"> View Course </v-btn>
+		<v-card-actions class="">
+			<v-btn color="#7E81FF">
+				<router-link :to="props.route"> View course </router-link>
+			</v-btn>
 		</v-card-actions>
 	</v-card>
 </template>
--- src/views/course/index.vue
+++ src/views/course/index.vue
@@ -1,111 +1,26 @@
 <template>
 	<div class="grid grid-cols-3 card-container gap-8">
 		<Card
-			v-for="(lang, index) in languages"
+			v-for="(lang, index) in data"
 			:key="index"
-			:image="lang.img"
-			:title="lang.title"
-			:card-text="lang.text"
+			class="h-full"
+			:image="lang.img !== '' ? lang.img : '/img1.png'"
+			:route="lang.path"
+			:title="lang.topic"
+			:card-text="lang.desc"
 		/>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed } from 'vue';
 import Card from 'course-components/CourseCard.vue';
+import { topics } from 'course-components/CourseTopics';
 
-const languages = ref([
-	{
-		title: 'Vue',
-		text: 'Learn more about vue',
-		img: '/img1.png',
-	},
-	{
-		title: 'Vue',
-		text: 'Learn more about vue',
-		img: '/img1.png',
-	},
-	{
-		title: 'Vue',
-		text: 'Learn more about vue',
-		img: '/img1.png',
-	},
-	{
-		title: 'Vue',
-		text: 'Learn more about vue',
-		img: '/img1.png',
-	},
-	{
-		title: 'Vue',
-		text: 'Learn more about vue',
-		img: '/img1.png',
-	},
-	{
-		title: 'Vue',
-		text: 'Learn more about vue',
-		img: '/img1.png',
-	},
-	{
-		title: 'Vue',
-		text: 'Learn more about vue',
-		img: '/img1.png',
-	},
-	{
-		title: 'Vue',
-		text: 'Learn more about vue',
-		img: '/img1.png',
-	},
-	{
-		title: 'Vue',
-		text: 'Learn more about vue',
-		img: '/img1.png',
-	},
-	{
-		title: 'Vue',
-		text: 'Learn more about vue',
-		img: '/img1.png',
-	},
-	{
-		title: 'Vue',
-		text: 'Learn more about vue',
-		img: '/img1.png',
-	},
-	{
-		title: 'Vue',
-		text: 'Learn more about vue',
-		img: '/img1.png',
-	},
-	{
-		title: 'Vue',
-		text: 'Learn more about vue',
-		img: '/img1.png',
-	},
-	{
-		title: 'Vue',
-		text: 'Learn more about vue',
-		img: '/img1.png',
-	},
-	{
-		title: 'Vue',
-		text: 'Learn more about vue',
-		img: '/img1.png',
-	},
-	{
-		title: 'Vue',
-		text: 'Learn more about vue',
-		img: '/img1.png',
-	},
-	{
-		title: 'Vue',
-		text: 'Learn more about vue',
-		img: '/img1.png',
-	},
-	{
-		title: 'Vue',
-		text: 'Learn more about vue',
-		img: '/img1.png',
-	},
-]);
+const data = computed(() => {
+	const newArr: any = [];
+	return newArr.concat(topics.languages, topics.frameworks);
+});
 </script>
 
 <style lang="scss" scoped>
