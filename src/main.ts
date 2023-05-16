a4d5a6499e71f46d19a6a3506aeef3ecc5128d81
HoKaeLok <hokaelok123@gmail.com>
2023-05-16 17:22:52 +0800

    hot fix: destructure overlarge imports

--- src/main.ts
+++ src/main.ts
@@ -36,13 +36,9 @@ library.add(
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import { aliases, fa } from 'vuetify/iconsets/fa';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 const vuetify = createVuetify({
-	components,
-	directives,
 	icons: {
 		defaultSet: 'fa',
 		aliases,
